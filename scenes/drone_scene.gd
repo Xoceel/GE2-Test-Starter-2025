87829b173491d14a680b7aec9a3bcab013d7b1e0
leeCox <14360547+leecox@user.noreply.gitee.com>
2025-04-29 09:10:55 +0100

    instantiating drone, add it, camera lerp to follow point

--- scenes/drone_scene.gd
+++ scenes/drone_scene.gd
@@ -1,22 +1,28 @@
 extends Node3D
 
 @onready var camera: Camera3D = $Camera3D
-@onready var drone: RigidBody3D = $Drone
 @onready var camera_follow_pos: Node3D = $Drone/CameraFollowPos
-
+const DRONE = preload("res://scenes/drone.tscn")
 var speed = 30
+var drone
 var lift = Vector3(0,5,0)
 var force = Vector3.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	drone = DRONE.instantiate()
+	drone.global_position = Vector3(0, 1, 0)
+	add_child(drone)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	camera.global_position = lerp(camera.position, camera_follow_pos.global_position, 1.0)
-	camera.look_at(drone.global_position)
-	if Input.is_action_pressed("up"):
-		force += lift
+	var follow_point = drone.global_position - Vector3(0, -5, 5)
 	
+	
+	camera.global_position = lerp(camera.position, follow_point, 1.0)
+	camera.look_at(drone.global_position)
+	#if Input.is_action_pressed("up"):
+		#force += lift
+	#
+	#velocit
