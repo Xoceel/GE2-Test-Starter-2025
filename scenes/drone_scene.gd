402b4f0c33b11ec6d4483724011e8f817e1e24f9
leeCox <14360547+leecox@user.noreply.gitee.com>
2025-04-29 08:50:40 +0100

    fixed that thing

--- scenes/drone_scene.gd
+++ scenes/drone_scene.gd
@@ -1,17 +1,19 @@
 extends Node3D
 
 @onready var camera: Camera3D = $Camera3D
-@onready var camera_follow_pos: Node3D = $DroneBody/CameraFollowPos
 @onready var drone: RigidBody3D = $Drone
+@onready var camera_follow_pos: Node3D = $Drone/CameraFollowPos
+
+var speed = 30
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(camera_follow_pos.global_position)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	camera.global_position = lerp(camera.position, Vector3(0,0,0), 1)
+	camera.global_position = lerp(camera.position, camera_follow_pos.global_position, .7)
 	camera.look_at(drone.global_position)
 	if Input.is_action_pressed("up"):
-		position.x += 1
+		position.y += speed * delta
